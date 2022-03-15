cae834bac4c35985533faad2c4f8a83edab7891e
Philippe Delodder <philippe@delodder.be>
2022-03-15 08:01:28 +0100

    Improvement for download script

--- docker/download-deconz.sh
+++ docker/download-deconz.sh
@@ -7,16 +7,13 @@ CHANNEL=$2
 PLATFORM=$3
 
 case "{$PLATFORM}" in
-  *linux/arm64*)
+  */arm64)
     URL="http://deconz.dresden-elektronik.de/debian/${CHANNEL}/deconz_${DECONZ_VERSION}-debian-buster-${CHANNEL}_arm64.deb"
     ;;
-  *linux/amd64*)
+  */amd64)
     URL="http://deconz.dresden-elektronik.de/ubuntu/${CHANNEL}/deconz-${DECONZ_VERSION}-qt5.deb"
     ;;
-  *linux/arm/v7*)
-    URL="http://deconz.dresden-elektronik.de/raspbian/${CHANNEL}/deconz-${DECONZ_VERSION}-qt5.deb"
-    ;;
-  *linux/arm/v6*)
+  */arm/v[67])
     URL="http://deconz.dresden-elektronik.de/raspbian/${CHANNEL}/deconz-${DECONZ_VERSION}-qt5.deb"
     ;;
 esac
